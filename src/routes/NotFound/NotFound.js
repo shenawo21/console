58baac46dd2a739861d0cc1cd2a0f543c00b1659
语晖 <yuhui@suneee.com>
2016-04-26 14:50:33 +0800

    404 page

--- src/routes/NotFound/NotFound.js
+++ src/routes/NotFound/NotFound.js
@@ -7,10 +7,24 @@ const goBack = (e) => {
 }
 
 export const NotFound = () => (
-  <div>
-    <h4>Page not found!</h4>
-    <p><a href='#' onClick={goBack}>&larr; Back</a></p>
-  </div>
+  <div className="page-error page-error-404 layout-full">
+        <div className="page animsition vertical-align text-center" data-animsition-in="fade-in" data-animsition-out="fade-out">
+          <div className="page-content vertical-align-middle">
+            <header>
+              <h1 className="animation-slide-top">404</h1>
+              <p>页面没有找到!</p>
+            </header>
+            <p className="error-advise">YOU SEEM TO BE TRYING TO FIND HIS WAY HOME</p>
+            <a className="btn btn-primary btn-round" href="#"  onClick={goBack}>GO BACK</a>
+            <footer className="page-copyright">
+              <p>WEBSITE BY amazingSurge</p>
+              <p>© 2015. All RIGHT RESERVED.</p>
+
+            </footer>
+          </div>
+        </div>
+      </div>
+
 )
 
 export default NotFound
