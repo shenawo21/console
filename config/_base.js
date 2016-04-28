34a802004a2f5812200499c2243e2fb3575b20c1
‘zegang’ <zegang@suneee.com>
2016-04-28 10:44:15 +0800

    修改开发模式下window资源找不到问题

--- config/_base.js
+++ config/_base.js
@@ -19,7 +19,7 @@ const config = {
   // ----------------------------------
   // Server Configuration
   // ----------------------------------
-  server_host: '0.0.0.0',
+  server_host: 'localhost',
   server_port: process.env.PORT || 3100,
 
   // ----------------------------------
