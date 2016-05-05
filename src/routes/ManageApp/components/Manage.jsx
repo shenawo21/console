5e43095a1f63c5a05b3c8265cb587280ccad254f
zeyu <zeyu@suneee.com>
2016-05-05 18:01:03 +0800

    Update

--- src/routes/ManageApp/components/Manage.jsx
+++ src/routes/ManageApp/components/Manage.jsx
@@ -1,6 +1,7 @@
 import React,{Component, PropTypes} from 'react';
 import {Link} from 'react-router';
-
+import MyAppList from '../routes/AppList/components/List'
+import TrashList from '../routes/TrashList/components/List'
 import { Tabs, Button,Icon } from 'hen';
 
 const TabPane = Tabs.TabPane;
@@ -13,8 +14,8 @@ class Manage extends Component{
   render() {
     return (
       <Tabs tabBarExtraContent={operations}>
-        <TabPane tab="我的应用" key="1"></TabPane>
-        <TabPane tab="回收站" key="2">回收站</TabPane>
+        <TabPane tab="我的应用" key="1"><MyAppList /></TabPane>
+        <TabPane tab="回收站" key="2"><TrashList /></TabPane>
       </Tabs>
     );
   }
