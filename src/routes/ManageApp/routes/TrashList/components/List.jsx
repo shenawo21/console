3b83dee3a609b02733cddb113fdadb7f8a0e02ac
zeyu <zeyu@suneee.com>
2016-05-05 17:31:40 +0800

    添加回收站列表

---
+++ src/routes/ManageApp/routes/TrashList/components/List.jsx
@@ -0,0 +1,22 @@
+import React,{Component, PropTypes} from 'react';
+import {Link} from 'react-router';
+import { Button, Row, Col, Input, InputNumber, DatePicker, message, Checkbox} from 'hen';
+
+class Add extends Component{
+
+  render() {
+    return (
+      <div>
+        <h2>回收站列表</h2>
+      </div>
+    );
+  }
+
+}
+
+Add.propsTypes = {
+
+}
+
+export default Add;
+
---
+++ src/routes/ManageApp/routes/TrashList/modules/list.js
@@ -0,0 +1,6 @@
+/**
+ * list.js
+ * @date Created on 2016/5/5
+ * @author Tity(泽钰)<zeyu@suneee.com>
+ *
+ */
