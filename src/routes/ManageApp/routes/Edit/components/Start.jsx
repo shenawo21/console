bd31c84bcaba9f71c309442b9e9ae46d8d5d3aed
shenxing <shenxing@suneee.com>
2016-05-10 17:37:51 +0800

    update

--- src/routes/ManageApp/routes/Edit/components/Start.jsx
+++ src/routes/ManageApp/routes/Edit/components/Start.jsx
@@ -8,6 +8,7 @@ const props = {
   showUploadList: false,
   action: '/upload.do',
 };
+
 export default () => {
   return <Row className={classes.edit}>
     <Col span="11">
@@ -22,11 +23,11 @@ export default () => {
       </Col>
       <Col span="6">
         <dl>
-          <dt>图片上传说明:</dt>
-          <dd>分辨率: <span className={classes.startColor}>768*1024</span></dd>
-          <dd>图片格式: <span className={classes.startColor}>png</span></dd>
-          <dd>图片大小: <span className={classes.startColor}>2M以内</span></dd>
-          <dd>适配机型: <span className={classes.startColor}>4寸以内小屏机</span></dd>
+          <dt className={classes.dt}>图片上传说明:</dt>
+          <dd className={classes.dd}>分辨率: <span className={classes.startColor}>768*1024</span></dd>
+          <dd className={classes.dd}>图片格式: <span className={classes.startColor}>png</span></dd>
+          <dd className={classes.dd}>图片大小: <span className={classes.startColor}>2M以内</span></dd>
+          <dd className={classes.dd}>适配机型: <span className={classes.startColor}>4寸以内小屏机</span></dd>
         </dl>
       </Col>
     </Row>
@@ -42,11 +43,11 @@ export default () => {
       </Col>
       <Col span="6">
         <dl>
-          <dt>图片上传说明:</dt>
-          <dd>分辨率: <span className={classes.startColor}>1440*2560</span></dd>
-          <dd>图片格式: <span className={classes.startColor}>png</span></dd>
-          <dd>图片大小: <span className={classes.startColor}>2M以内</span></dd>
-          <dd>适配机型: <span className={classes.startColor}>4寸以上大屏机</span></dd>
+          <dt className={classes.dt}>图片上传说明:</dt>
+          <dd className={classes.dd}>分辨率: <span className={classes.startColor}>1440*2560</span></dd>
+          <dd className={classes.dd}>图片格式: <span className={classes.startColor}>png</span></dd>
+          <dd className={classes.dd}>图片大小: <span className={classes.startColor}>2M以内</span></dd>
+          <dd className={classes.dd}>适配机型: <span className={classes.startColor}>4寸以上大屏机</span></dd>
         </dl>
       </Col>
     </Row>
