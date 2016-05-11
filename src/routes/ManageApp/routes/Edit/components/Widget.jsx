54b7857248b477d2d16e52bae5270f0873dbe7d5
shenxing <shenxing@suneee.com>
2016-05-11 14:54:18 +0800

    update

--- src/routes/ManageApp/routes/Edit/components/Widget.jsx
+++ src/routes/ManageApp/routes/Edit/components/Widget.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {Row, Col,Upload, Icon} from 'hen';
+import classes from '../assets/Edit.less';
+import startImage from '../assets/startImg.png'
 export default () => {
-  return <div>
-    功能设置
-  </div>
+  return <Row className={classes.edit}>
+    <Col span="11">
+        <div className={classes.imgSize}>自定义导航</div>
+
+    </Col>
+    <Col span="13" >
+      <div className={classes.mobShell}>
+        <div className={classes.mobScreen}>
+          <img src={startImage} width="240" height="427" />
+        </div>
+      </div>
+    </Col>
+  </Row>
 }
 
 
