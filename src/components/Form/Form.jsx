e6ab417b953ba50a22bc3c6b8f8f5399afba0e51
‘zegang’ <zegang@suneee.com>
2016-05-24 20:21:50 +0800

    修改checkboxgroup配置

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -206,7 +206,7 @@ class Forms extends Component {
     
     //  checkboxGroup 复选框
     if (item.checkboxGroup) {
-        <CheckboxGroup {...fieldProps} {...item.checkbox}  />
+        return <CheckboxGroup {...fieldProps} {...item.checkboxGroup}  />
     }
     
     //数值文本框
