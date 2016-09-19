aa73891ed36f08a016bf47211aae8688decf311a
zeyu <zeyu@suneee.com>
2016-09-19 13:56:40 +0800

    出库店铺限制问题

--- src/routes/Virtualhouse/routes/OutgoMgt/components/OutgoMgtView.jsx
+++ src/routes/Virtualhouse/routes/OutgoMgt/components/OutgoMgtView.jsx
@@ -344,7 +344,7 @@ class outgoMgt extends Component {
                         {
                             shopList && shopList.map((val, i) => {
                                typeof val.value === 'boolean' && (val.value = '' + val.value);
-                                return <Option key={i} value={val.value} disabled={(val.status != 'use' || val.enabled) ? true : false}>{val.title}</Option>
+                                return <Option key={i} value={val.value} disabled={val.disabled}>{val.title}</Option>
                             })
                         }
                     </Select>
